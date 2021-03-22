6953299f834bd6ac66ce19b6d2dd0789a358c8ec
mitchell-alexleigh <75285458+mitchell-alexleigh@users.noreply.github.com>
2021-03-22 12:16:48 -0500

    Update Charts and Index

--- charts.js
+++ charts.js
@@ -57,30 +57,48 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
+    var samples = data.samples;
 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    
     //  5. Create a variable that holds the first sample in the array.
-
+    var result = resultArray[0];
+      //console.log(result)
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otu_ids = result.otu_ids.toString()
+      console.log('otu_ids : '+otu_ids.slice(0,10))
+    var otu_labels = result.otu_labels
+      console.log('otu_labels : '+otu_labels.slice(0,10))
+    var sample_values = result.sample_values
+  console.log('sample_values : '+sample_values.slice(0,10))
 
+    
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
-
-    var yticks = 
+  
+    var yticks = otu_ids.slice(0,10);
+      //console.log('sortedSamples '+top_10_otu_ids)
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var barData = [{
+      x: sample_values.slice(0,10),
+      y: otu_ids.slice(0,10),
+      text: otu_labels.slice(0,10),
+      type: "bar",
+      orientation: 'h',
+      }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "Top 10 Bacteria Foung",
+      xaxis: { title: "Bacteria"},
+      yaxis: { title: "ID", tickvals: yticks},
+      
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar", barData, barLayout )
   });
 }
